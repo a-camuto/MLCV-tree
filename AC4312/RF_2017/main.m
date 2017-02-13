06d90863e2ef802a2c77bd0d3b2bb0c33ec61184
Alexandre Camuto <alexandrecamuto@dyn1205-222.wlan.ic.ac.uk>
2017-02-13 17:36:00 +0000

    main.m update
    main.m update

--- AC4312/RF_2017/main.m
+++ AC4312/RF_2017/main.m
@@ -7,8 +7,21 @@ T=4; % number of data subsets
 replacement=true;
 S_t=bagging(T,data_train,replacement,20);
 
+param.num = 1;         % Number of trees
+param.depth = 5;        % trees depth
+param.splitNum = 3;     % Number of split functions to try
+param.split = 'IG';     % Currently support 'information gain' only
 
 for i= 1:T
     figure
 plot_toydata(S_t(:,:,i));
+trees(i) = growTrees(S_t(:,:,i),param);
 end 
+
+
+
+
+%%%%%%%%%%%%%%%%%%%%%%
+% Train Random Forest
+
+% Grow all trees
