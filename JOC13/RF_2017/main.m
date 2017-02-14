19083dde261046dffda113b1445d4549a95105af
Jonathan Campbell <jonathan.o.campbell@gmail.com>
2017-02-14 16:39:09 +0000

    changed my main file

--- JOC13/RF_2017/main.m
+++ JOC13/RF_2017/main.m
@@ -1,29 +1,17 @@
+% Jack's main file
+
 %% Load data
+clear; 
 init;
-[data_train, data_test] = getData('Toy_Spiral');
+[data_train, data_test] = getData('Toy_Spiral');  
+ 
+%% Grow trees
 
-%% Bagging
-T=4; % number of data subsets
-n_t=20
-replacement=true;
-%S_t=bagging(T,data_train,replacement,n_t);
-S_t = data_train
+param.num = 4;
+param.depth = 2;
+param.splitNum = 3;
+param.split = 'IG';
 
-param.num = 1;         % Number of trees
-param.depth = 2;        % trees depth
-param.splitNum = 1;     % Number of split functions to try
-param.split = 'none';     % Currently support 'information gain' only
-%% Growing trees
-for i= 1:T
-    
-    figure
-    plot_toydata(S_t(:,:,i));
-    trees(i) = growTrees(S_t(:,:,i),param);
-    idx = trees(1).node(2).idx;
-    NS_t = zeros(length(idx),size(S_t,2));
-    for j = 1:length(idx)
-        NS_t(j,:)= S_t(idx(j),:,i);
-    end
-    figure
-    plot_toydata(NS_t)
-end 
+plot_toydata(data_train);
+trees = growTrees(data_train,param);
+
